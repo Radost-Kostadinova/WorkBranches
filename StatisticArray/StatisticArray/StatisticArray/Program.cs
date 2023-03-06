8ebf95e292bc7399f72a51e97664dd5e7830a7ab
Radost-Kostadinova <Radost-Kostadinova>
2023-03-06 10:36:59 +0200

    printOddNums

--- StatisticArray/StatisticArray/StatisticArray/Program.cs
+++ StatisticArray/StatisticArray/StatisticArray/Program.cs
@@ -19,7 +19,7 @@ namespace StatisticArray
             OptionsManager.PrintArr(arr);
             // TO DO
             OptionsManager.SortOut(arr);
-            
+            OptionsManager.PrintOddsNum(arr);
             OptionsManager.PrintArr(arr);
 
         }
