061cd4dfec0a5ee813f4414ff2a21f8afba65a53
Radost-Kostadinova <Radost-Kostadinova>
2023-03-06 10:28:52 +0200

    inisial commit

---
+++ StatisticArray/StatisticArray/StatisticArray/OptionsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticArray
+{
+   public static class OptionsManager
+    {
+        public static int CountLess100(int[] arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+
+        public static int MaxNum(int[] arr)
+        {
+            return arr.Max();
+        }
+
+        public static int[] ReverseArr(int[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+
+        public static void PrintArr(int[]arr)
+        {
+            Console.WriteLine(string.Join(", ",arr));
+        }
+
+        //Task1
+        public static void SortOut(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 1).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+ 
+        public static void PrintOddsNum(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 1).ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+
+        //task2
+        public static int CountOddNumbers(int[] arr)
+        {
+            return arr.Where(x => x % 2 == 1).Count();
+        }
+
+    }
+}
